470cedc209933b4fa4a3117c728c36fd6c682c4b
Константин Шарыгин <kot.074@mail.ru>
2022-12-25 15:26:59 +0300

    Перенес настройки Swagger и JWT в методы расширения.

---
+++ Saturn.UsersService/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Saturn.UsersService.Auth;
+
+namespace Saturn.UsersService.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        internal static IServiceCollection AddApplicationSwagger(this IServiceCollection services) => services
+            .AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "bearer"
+                });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference {Type = ReferenceType.SecurityScheme, Id = "Bearer"}
+                        },
+                        new string[] { }
+                    }
+                });
+            });
+
+        internal static void AddApplicationAuthentication(this IServiceCollection services)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.RequireHttpsMetadata = false;
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidIssuer = AuthOptions.ISSUER,
+
+                        ValidateAudience = true,
+                        ValidAudience = AuthOptions.AUDIENCE,
+
+                        ValidateLifetime = false,
+
+                        IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+                        ValidateIssuerSigningKey = true
+                    };
+                });
+        }
+    }
+}
--- Saturn.UsersService/Program.cs
+++ Saturn.UsersService/Program.cs
@@ -1,7 +1,4 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
-using Saturn.UsersService.Auth;
+using Saturn.UsersService.Extensions;
 using Saturn.UsersService.Repositories;
 
 namespace Saturn.UsersService
@@ -25,49 +22,10 @@ namespace Saturn.UsersService
 
             builder.Services.AddControllers();
             builder.Services.AddScoped<IUsersRepository, UsersRepository>();
-
-            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(options =>
-                {
-                    options.RequireHttpsMetadata = false;
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuer = true,
-                        ValidIssuer = AuthOptions.ISSUER,
-
-                        ValidateAudience = true,
-                        ValidAudience = AuthOptions.AUDIENCE,
-
-                        ValidateLifetime = false,
-
-                        IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
-                        ValidateIssuerSigningKey = true
-                    };
-                });
-
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen(options =>
-            {
-                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                {
-                    In = ParameterLocation.Header,
-                    Description = "Please enter token",
-                    Name = "Authorization",
-                    Type = SecuritySchemeType.Http,
-                    BearerFormat = "JWT",
-                    Scheme = "bearer"
-                });
 
-                options.AddSecurityRequirement(new OpenApiSecurityRequirement
-                {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" }
-                        }, new string[]{}
-                    }
-                });
-            });
+            builder.Services.AddApplicationAuthentication();
+            builder.Services.AddApplicationSwagger();
 
             var app = builder.Build();
 
